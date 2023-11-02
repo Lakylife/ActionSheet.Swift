071f5a3fe6b32211aee78bb3358f738aa5d263cc
Walletpayin <122436967+Lakylife@users.noreply.github.com>
2023-11-02 13:41:50 +0100

    Delete Sources/ActionSheet directory

--- Sources/ActionSheet/GrouviActionSheet.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-public class GrouviActionSheet {
-    public static let shared = GrouviActionSheet()
-    private init() {}
-
-    static public let bundle = Bundle(for: GrouviActionSheet.self)
-}
